269923b161e37e0152672dc608bd3633abf911c0
Mateus Matos de Oliveira <mateus.m.oliveira2303@gmail.com>
2024-05-27 23:54:31 -0300

    inserido um registro finalmente

--- index.py
+++ index.py
@@ -1,43 +1,76 @@
-import sqlite3 as sql
-from sqlite3 import Error
+import mysql.connector as sql
+from mysql.connector import Error
 
 
-def criar_conexao_bd(caminho):
+def criar_conexao_bd(host_name, user_name, user_pswd):
     conexao = None
     try:
-        conexao = sql.connect(caminho)
-        print("Conexão estabelecida com sucesso ao bd")
+        conexao = sql.connect(
+                host=host_name,
+                user=user_name,
+                passwd=user_pswd
+        )
+        print("Conexão com banco de dados feita com sucesso")
     except Error:
         print(f"Erro: {Error}")
+
     return conexao
 
 
-bd = criar_conexao_bd("teste.db")
-
-
-def executar_comando(conexao, comando, valores):
+def criar_database(conexao):
+    sql_criar_database = """
+        CREATE DATABASE Tradicional_Public_Schools;
+    """
     cursor = conexao.cursor()
     try:
-        cursor.execute(comando, valores)
+        cursor.execute(sql_criar_database)
+        print("Database criada com sucesso")
+    except Error:
+        print(f"Erro: {Error}")
+
+
+def usar_database(conexao):
+    sql_usar_database = """
+            USE Tradicional_Public_Schools;
+        """
+    cursor = conexao.cursor()
+    try:
+        cursor.execute(sql_usar_database)
+        print("Database usada com sucesso")
+    except Error:
+        print(f"Erro: {Error}")
+
+
+def executar_comando(conexao, comando):
+    cursor = conexao.cursor()
+    try:
+        cursor.execute(comando)
         conexao.commit()
         print("Comando feito com sucesso")
     except Error:
         print(f"Erro: {Error}")
 
 
-def inserir_aluno(conexao):
-    cursor = conexao.cursor()
-    nome = input("Qual o nome do aluno: ")
-    data_nascimento = input("Data de nascimento: ")
-    cpf = input("CPF: ")
-    valores = (nome, data_nascimento, cpf)
+sql_criar_tabela_usuario = """
+    CREATE TABLE Usuario(
+        id int primary key auto_increment,
+        nome varchar(100),
+        tipo_usuario varchar(30),
+        data_nascimento date,
+        cpf varchar(11)
+        );
+"""
 
-    sql_inserir_aluno = """
-    INSERT INTO Aluno(id, nome, data_nascimento, cpf) VALUES
-    (2, ?, ?, ?)
-    """
-    cursor.execute(sql_inserir_aluno, valores)
-    conexao.commit()
+sql_inserir_usuario = """
+    INSERT INTO Usuario (nome, tipo_usuario, data_nascimento, cpf) VALUES
+    (%s, %s, %s, %s);
+"""
+valores = ("mateus", "admin", "2002/11/23", "123")
 
+conexao_bd = criar_conexao_bd("localhost", "root", "Gote03/18")
+usar_database(conexao_bd)
+cursor = conexao_bd.cursor()
+cursor.execute(sql_inserir_usuario, valores)
+conexao_bd.commit()
 
-inserir_aluno(bd)
+# executar_comando(conexao_bd, sql_inserir_usuario)
